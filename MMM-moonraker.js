42c0c9e4fad25b215f9e11d6134314fa9e0929b9
sam detweiler <sdetweil@gmail.com>
2023-05-28 08:46:03 -0500

    remove node_helper

--- MMM-moonraker.js
+++ MMM-moonraker.js
@@ -16,6 +16,8 @@ Module.register("MMM-moonraker", {
     hideDataOnStandby: true
   },
 
+  // hold timer id
+  fetchTimerId: null,
   // Store the data in an object.
   displayData: {
     printer_status: null,
@@ -30,26 +32,16 @@ Module.register("MMM-moonraker", {
 
   start: function () {
     Log.info(`Starting module: ${this.name}`);
-    this.sendSocketNotification("CONFIG", this.config);
+    if (this.fetchTimerId) {
+      clearTimeout(this.fetchTimerId);
+    }
+
+    this.fetchData();
   },
 
-  socketNotificationReceived: function (notification, payload) {
-    switch (notification) {
-      case "PRINTER_STATUS":
-        this.displayData = payload;
-        this.available = true;
-        this.loading = false;
-        break;
-      case "HTTP_ERROR":
-        this.loading = false;
-        this.available = false;
-        break;
-    }
-    this.updateDom(this.config.animationSpeed);
-  },
 
   getTemplate: function () {
-    return "templates\\mmm-moonraker.njk";
+    return "templates/"+this.name+".njk";
   },
 
   getTemplateData() {
@@ -71,7 +63,7 @@ Module.register("MMM-moonraker", {
 
   getStyles: function () {
     return [
-      "MMM-moonraker.css",
+      this.name+".css"
     ];
   },
 
@@ -80,5 +72,98 @@ Module.register("MMM-moonraker", {
       en: "translations/en.json",
       de: "translations/de.json",
     };
+  },
+
+  async fetchData() {
+    const self = this;
+
+    const printer_status = await this.fetchPrinterStatus();
+    if(printer_status){
+      if (this.currentFile !== printer_status.print_stats.filename) {
+        this.currentFile = printer_status.print_stats.filename;
+        this.metadata = await this.fetchMetadata(printer_status);
+      }
+
+      const eta = await this.calculateEta(printer_status, this.metadata);
+
+      this.process_return("PRINTER_STATUS", { printer_status, metadata: this.metadata, eta });
+
+      this.fetchTimerId = setTimeout(async function () {
+        await self.fetchData();
+      }, this.config.updateInterval);
+    }
+  },
+
+
+  async fetchPrinterStatus() {
+    const endpoint = this.config.endpoint + "/printer/objects/query?print_stats&virtual_sdcard&extruder&heater_bed";
+
+    try {
+      const response = await fetch(endpoint);
+      const json = await response.json();
+
+      return json.result.status;
+    } catch (error) {
+      Log.log(`${this.name} received an error: ${error}`);
+
+      this.process_return("HTTP_ERROR", {});
+
+      return null;
+    }
+  },
+
+  process_return(notification, payload){
+    switch (notification) {
+      case "PRINTER_STATUS":
+        this.displayData = payload;
+        this.available = true;
+        this.loading = false;
+        break;
+      case "HTTP_ERROR":
+        this.loading = false;
+        this.available = false;
+        break;
+    }
+    this.updateDom(this.config.animationSpeed);
+
+  },
+
+  async fetchMetadata(printer_status) {
+    const endpoint = this.config.endpoint + "/server/files/metadata?filename=" + printer_status.print_stats.filename;
+
+    try {
+      const response = await fetch(endpoint);
+      const json = await response.json();
+
+      // join up the final thumbnail url for convenience
+      for (let index = 0; index < json.result.thumbnails.length; index++) {
+        const element = json.result.thumbnails[index];
+        element.relative_path = this.config.endpoint + "/server/files/gcodes/" + element.relative_path;
+      }
+
+      // sort thumbnails by width so the first one is the larger one
+      json.result.thumbnails.sort(function (a, b) { return b.width - a.width });
+
+      return json.result;
+    } catch (error) {
+      Log.log(`${this.name} received an error: ${error}`);
+      this.process_return("HTTP_ERROR", {});
+
+      return null;
+    }
+  },
+
+  calculateEta(printer_status, metadata) {
+    if (printer_status.print_stats.state !== 'printing') {
+      return null;
+    }
+
+    if (metadata && metadata.estimated_time) {
+      const progressTime = printer_status.virtual_sdcard.progress * metadata.estimated_time;
+      return moment.utc(1000 * (metadata.estimated_time - progressTime)).format('HH[h] mm[m] ss[s]');;
+    }
+
+    const total_time = printer_status.print_stats.print_duration / printer_status.virtual_sdcard.progress;
+    return moment.utc(1000 * (total_time - printer_status.print_stats.print_duration)).format('HH[h] mm[m] ss[s]');
   },
 });
